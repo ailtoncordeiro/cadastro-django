d74ab6fb337d6405e211911514903c76e53738a5
Ailton Cordeiro <ailtoncordeiro@gmail.com>
2025-02-04 22:56:07 -0300

    feat: adicionando relacionamentos

--- cadastro/models.py
+++ cadastro/models.py
@@ -9,10 +9,21 @@ class TimeStamp(models.Model):
     class Meta:
         abstract = True
 
+class Infracao(TimeStamp):
+    lei = models.CharField(max_length=255, verbose_name='Lei')
+    artigo = models.CharField(max_length=255, verbose_name='Artigo')
+    descricao = models.CharField(max_length=255, verbose_name='Descricao')
+
 class Pessoa(TimeStamp):
     cpf = models.CharField(max_length=14, unique=True, verbose_name='CPF')
     nome = models.CharField(max_length=255, verbose_name='Nome')
     email = models.CharField(max_length=255, verbose_name='e-mail', null=True)
+    #Relacionamento de Muitos para Muitos com Pessoa
+    infracoes = models.ManyToManyField(
+        Infracao,
+        blank=True,
+        related_name='pessoas'
+    )
 
     # Campo que será exibido na lista da pagina de administração do django
     def __str__(self):
@@ -20,3 +31,12 @@ class Pessoa(TimeStamp):
 
     class Meta:
         ordering = ['nome']
+
+class Foto(TimeStamp):
+    foto = models.ImageField(upload_to='fotos/')
+    pessoa = models.ForeignKey(
+        Pessoa,
+        on_delete=models.CASCADE,
+        related_name='fotos'
+    )
+
